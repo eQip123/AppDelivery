ff7ff03bb3b6a16c0319caadf7ea2bdc1d0e4165
eQip <aidarasanakunov@gmail.com>
2022-10-28 16:03:01 +0600

    added TableViewCell and fix a buges tableView

--- DeliveryApp/MainModule/View/MainViewController.swift
+++ DeliveryApp/MainModule/View/MainViewController.swift
@@ -14,17 +14,19 @@ class MainViewController: UIViewController {
     }()
     
     private lazy var tableView: UITableView = {
-        let view = UITableView()
-        view.layer.borderColor = UIColor.gray.cgColor
-        view.rowHeight = 83
-        return view
+        let tableView = UITableView()
+        tableView.register(MainTableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.layer.borderColor = UIColor.gray.cgColor
+        tableView.rowHeight = 83
+        return tableView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupViews()
         setupConstraints()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     func setupViews() {
         view.backgroundColor = .white
@@ -43,5 +45,24 @@ class MainViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-22)
             make.bottom.equalToSuperview().offset(-4)
         }
+    }
+}
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MainTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.firstTitleLabel.text = "Aidar"
+        cell.secondTitleLabel.text = "Aidar"
+        return cell
+    }
+}
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
